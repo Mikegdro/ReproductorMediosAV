fa55b7ecb7fa86467f1dd64e58f45a4751fb6469
Mike <miguelgdro@gmail.com>
2022-06-15 02:11:57 +0200

    Añadiendo Main

--- src/Main/Reproductor.java
+++ src/Main/Reproductor.java
@@ -1,18 +1,32 @@
 package Main;
 
+import Clases.ListaMedios;
 import javafx.application.Application;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
 import javafx.stage.Stage;
 
 public class Reproductor extends Application{
-
     
-    public static void main(String[]args){
-        
-    }
+    /*
+    Objetos JavaFX necesarios para la reproducción del video
+    */
+    private Media aReproducir;
+    private MediaPlayer reproductor;
+    private MediaView vistaReproductor;
+    
+    /*
+    Objeto ListaMedios que cargaremos con los archivos manipulados recientemente
+    */
+    private ListaMedios recientes;
     
     @Override
     public void start(Stage stage) throws Exception {
         
     }
     
+    public static void main(String[]args){
+        launch(args);
+    }
 }
